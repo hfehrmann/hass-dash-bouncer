d7e7fd1e086b5b4504e77ab94d5937a144223fd4
Hans Fehrmann <hans.jfehrmann@gmail.com>
2026-01-03 17:47:25 -0800

    Remove reload and entry unload

--- custom_components/dash_bouncer/__init__.py
+++ custom_components/dash_bouncer/__init__.py
@@ -24,18 +24,6 @@ async def async_setup_entry(
     _register_side_panel(hass)
     await _async_register_api_call(hass)
 
-    def relaod_dash_bouncer(call: ServiceCall) -> None:
-        """Handle reload of the integration."""
-    hass.services.async_register(DOMAIN, SERVICE_RELOAD, relaod_dash_bouncer)
-
-    return True
-
-async def async_unload_entry(
-        hass: HomeAssistant,
-        entry: ConfigEntry, # noqa: ARG001
-) -> bool:
-    """Unload a config entry."""
-    async_remove_panel(hass, frontend_url_path=DOMAIN)
     return True
 
 def _register_side_panel(hass: HomeAssistant) -> None:
