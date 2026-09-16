a1b29a9428baac42550cefe95b277aed5160509e
Hans Fehrmann <hans.jfehrmann@gmail.com>
2026-09-16 01:15:08 -0700

    Feat/conf reload (#7)
    Adds configuration reload on the frontend
    
    - Created _admin decorator for views that blocks non admin users
    - Moved file parsing into a general location.
       - Using async loop to not block main thread when reading file
    - Use that on init and on new reload view
    - Wire new button in frontend to the new view API
    <img width="1390" height="541" alt="Screenshot 2026-09-16 at 1 04 06 AM"
    src="https://github.com/user-attachments/assets/d90c371d-633d-4204-8069-046f3b7397f2"
    />
    
    
    
    ### TODO
    present some kind of toast when it succeed or fail.

--- custom_components/dash_bouncer/views.py
+++ custom_components/dash_bouncer/views.py
@@ -1,8 +1,9 @@
 """REST endpoints for DashBouncer."""
 import asyncio
 import logging
+from collections.abc import Callable
 from pathlib import Path
-from typing import Any, cast
+from typing import ParamSpec, cast
 
 import yaml
 from aiohttp import web
@@ -13,99 +14,25 @@ from homeassistant.core import HomeAssistant
 
 from .config import Config, RoleConfig, UserConfig, integration_config_path
 from .const import DOMAIN, PERMANENT_PANELS
-from .util import get_system_default_panel
+from .util import get_system_default_panel, load_config_from_file
 
 _LOGGER = logging.getLogger(__name__)
 
-class DashBouncerPanelsView(HomeAssistantView):
-    """Endpoint for getting all the panels availables from HASS."""
 
-    url = "/api/dash_bouncer/panels"
-    name = "api:dash_bouncer:panels"
-    requires_auth = True
-
-    async def get(self, request: web.Request) -> web.Response:
-        """Get the panels."""
-        hass = request.app["hass"]
+P = ParamSpec("P")
+def _admin[T](
+    wrapper: Callable[[T, web.Request, HomeAssistant], web.Response]
+) -> Callable[[T, web.Request], web.Response]:
+    async def check(self: T, request: web.Request, **kwargs: P.kwargs) -> web.Response:
         user = request["hass_user"]
-
         if not user.is_admin:
             return self.json({
                 "error": "Admin access required",
                 "message": "Only administrators can access panels information",
                 "redirect_url": "/"
             }, status_code=403)
-
-        try:
-            all_panels = hass.data[DATA_PANELS]
-            panels = [
-                all_panels[key].to_response()
-                for key in all_panels if key not in PERMANENT_PANELS
-            ]
-
-            return self.json(panels)
-        except Exception as e:
-            _LOGGER.exception("Error getting panels")
-            return self.json({"error": str(e)}, status_code=500)
-
-        return None
-
-class DashBouncerUsersView(HomeAssistantView):
-    """Endpoint for getting all the registered users from HASS."""
-
-    url = "/api/dash_bouncer/users"
-    name = "api:dash_bouncer:users"
-    requires_auth = True
-
-    async def get(self, request: web.Request) -> web.Response:
-        """Get the panels."""
-        hass = request.app["hass"]
-        user = request["hass_user"]
-
-        if not user.is_admin:
-            return self.json({
-                "error": "Admin access required",
-                "message": "Only administrators can access users information",
-                "redirect_url": "/"
-            }, status_code=403)
-
-        try:
-            _, storage, _ = hass.data[PERSON_DOMAIN]
-
-            return self.json(storage.async_items())
-        except Exception as e:
-            _LOGGER.exception("Error getting users")
-            return self.json({"error": str(e)}, status_code=500)
-
-        return None
-
-class DashBouncerConfigView(HomeAssistantView):
-    """Endpoint for getting the bouncer config."""
-
-    url = "/api/dash_bouncer/config"
-    name = "api:dash_bouncer:config"
-    requires_auth = True
-
-    async def get(self, request: web.Request, user: Any = None) -> web.Response:
-        """Get the config for the system."""
-        hass = request.app["hass"]
-        user = request["hass_user"]
-
-        if not user.is_admin:
-            return self.json({
-                "error": "Admin access required",
-                "message": "Only administrators can access config information",
-                "redirect_url": "/"
-            }, status_code=403)
-
-        try:
-            config = cast("Config | None", hass.data.get(DOMAIN))
-            return self.json(None if config is None else config.dump())
-        except Exception as e:
-            _LOGGER.exception("Error getting config")
-            return self.json({"error": str(e)}, status_code=500)
-
-        return None
+        return await wrapper(self, request, **kwargs)
+    return check
 
 class DashBouncerConfigSaveableMixin:
     """Mixin for saving a config."""
@@ -133,6 +60,100 @@ class DashBouncerConfigSaveableMixin:
         hass.data[DOMAIN] = new_config
         return self.json(new_config)
 
+class DashBouncerPanelsView(HomeAssistantView):
+    """Endpoint for getting all the panels availables from HASS."""
+
+    url = "/api/dash_bouncer/panels"
+    name = "api:dash_bouncer:panels"
+    requires_auth = True
+
+    @_admin
+    async def get(self, request: web.Request) -> web.Response:
+        """Get the panels."""
+        hass = request.app["hass"]
+        try:
+            all_panels = hass.data[DATA_PANELS]
+            panels = [
+                all_panels[key].to_response()
+                for key in all_panels if key not in PERMANENT_PANELS
+            ]
+
+            return self.json(panels)
+        except Exception as e:
+            _LOGGER.exception("Error getting panels")
+            return self.json({"error": str(e)}, status_code=500)
+
+        return None
+
+class DashBouncerUsersView(HomeAssistantView):
+    """Endpoint for getting all the registered users from HASS."""
+
+    url = "/api/dash_bouncer/users"
+    name = "api:dash_bouncer:users"
+    requires_auth = True
+
+    @_admin
+    async def get(self, request: web.Request) -> web.Response:
+        """Get the panels."""
+        hass = request.app["hass"]
+        try:
+            _, storage, _ = hass.data[PERSON_DOMAIN]
+
+            return self.json(storage.async_items())
+        except Exception as e:
+            _LOGGER.exception("Error getting users")
+            return self.json({"error": str(e)}, status_code=500)
+
+        return None
+
+class DashBouncerConfigView(HomeAssistantView):
+    """Endpoint for getting the bouncer config."""
+
+    url = "/api/dash_bouncer/config"
+    name = "api:dash_bouncer:config"
+    requires_auth = True
+
+    @_admin
+    async def get(self, request: web.Request) -> web.Response:
+        """Get the config for the system."""
+        hass = request.app["hass"]
+        try:
+            config = cast("Config | None", hass.data.get(DOMAIN))
+            return self.json(None if config is None else config.dump())
+        except Exception as e:
+            _LOGGER.exception("Error getting config")
+            return self.json({"error": str(e)}, status_code=500)
+
+        return None
+
+class DashBouncerReloadConfigView(HomeAssistantView):
+    """Endpoint for reloading the bouncer config."""
+
+    url = "/api/dash_bouncer/reload_config"
+    name = "api:dash_bouncer:relaad_config"
+    requires_auth = True
+
+    @_admin
+    async def post(self, request: web.Request) -> web.Response:
+        """Reload the config from storage."""
+        hass = request.app["hass"]
+        try:
+
+            old_config = cast("Config | None", hass.data.get(DOMAIN))
+            config = await load_config_from_file(hass)
+
+            if config is None:
+                config = old_config
+
+            hass.data[DOMAIN] = config
+
+            return self.json(None if config is None else config.dump())
+        except Exception as e:
+            _LOGGER.exception("Error getting config")
+            return self.json({"error": str(e)}, status_code=500)
+
+        return None
+
 class DashBouncerUserConfigView(HomeAssistantView, DashBouncerConfigSaveableMixin):
     """Endpoint for getting the bouncer config."""
 
@@ -140,18 +161,10 @@ class DashBouncerUserConfigView(HomeAssistantView, DashBouncerConfigSaveableMixi
     name = "api:dash_bouncer:config:user"
     requires_auth = True
 
+    @_admin
     async def post(self, request: web.Request, user_id: str) -> web.Response:
         """Set config for user."""
         hass = request.app["hass"]
-        user = request["hass_user"]
-
-        if not user.is_admin:
-            return self.json({
-                "error": "Admin access required",
-                "message": "Only administrators can access config information",
-                "redirect_url": "/"
-            }, status_code=403)
-
         try:
             config = cast("Config | None", hass.data.get(DOMAIN))
 
@@ -176,18 +189,10 @@ class DashBouncerRoleConfigView(HomeAssistantView, DashBouncerConfigSaveableMixi
     name = "api:dash_bouncer:config:role"
     requires_auth = True
 
+    @_admin
     async def post(self, request: web.Request, role_id: str) -> web.Response:
         """Set or create config for role."""
         hass = request.app["hass"]
-        user = request["hass_user"]
-
-        if not user.is_admin:
-            return self.json({
-                "error": "Admin access required",
-                "message": "Only administrators can access config information",
-                "redirect_url": "/"
-            }, status_code=403)
-
         try:
             config = cast("Config | None", hass.data.get(DOMAIN))
 
@@ -205,18 +210,10 @@ class DashBouncerRoleConfigView(HomeAssistantView, DashBouncerConfigSaveableMixi
 
         return None
 
+    @_admin
     async def delete(self, request: web.Request, role_id: str) -> web.Response:
         """Delete role."""
         hass = request.app["hass"]
-        user = request["hass_user"]
-
-        if not user.is_admin:
-            return self.json({
-                "error": "Admin access required",
-                "message": "Only administrators can access config information",
-                "redirect_url": "/"
-            }, status_code=403)
-
         try:
             config = cast("Config | None", hass.data.get(DOMAIN))
 
